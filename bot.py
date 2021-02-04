ea2b08835658ad887eb139c5d14018e1f55427e5
Emanuel Huerga <emanuel_cehc_95@hotmail.com>
2021-02-03 22:34:19 -0300

    prueba

--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
 from telegram.ext import Updater, CommandHandler
 
 def start(update,context):
-    update.message.reply_text('Hola Flor!.... Estoy vivo!!')
+    update.message.reply_text('Hola!.... Estoy vivo!!')
+
+def bye(update,context):
+    update.message.reply_text('a la camita... a mimirr...')
+    
 
 if __name__ == '__main__':
     updater = Updater(token='1696726088:AAEqIJbF61Y9p9LWxE7tvaGca_pJ5P_8t-w', use_context=True)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler('start',start))
+    dp.add_handler(CommandHandler('bye',bye))
 
     updater.start_polling()
     updater.idle()
