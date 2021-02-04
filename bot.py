fc8dea46ed6f722b6c7360d5a5f0299f28c61f71
Emanuel Huerga <emanuel_cehc_95@hotmail.com>
2021-02-03 23:15:37 -0300

    Amimir.jpg agregado

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ChatAction
 
 def start(update,context):
-    update.message.reply_text('Hola!.... Estoy vivo!!')
+    update.message.reply_text('Hola, '+str(update.message.chat.username)+'!.... Estoy vivo!!')
 
 def bye(update,context):
     update.message.reply_text('a la camita... a mimirr...')
+    chat = update.message.chat
+    chat.send_photo(
+        photo = open("amimir.jpg",'rb')
+    )
     
 
 if __name__ == '__main__':
